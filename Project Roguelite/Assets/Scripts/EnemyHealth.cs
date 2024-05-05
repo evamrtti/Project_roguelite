dda4843cf7a6fe1e2eba985f481fcc9075eb6cfc
evamrtti <98110786+evamrtti@users.noreply.github.com>
2024-05-06 00:53:11 +0200

    Update EnemyHealth.cs

--- Project Roguelite/Assets/Scripts/EnemyHealth.cs
+++ Project Roguelite/Assets/Scripts/EnemyHealth.cs
@@ -6,7 +6,7 @@ public class EnemyHealth : MonoBehaviour
 {
     public float currentHealth;
     public float maxHealth;
-    // Start is called before the first frame update
+
     void Start()
     {
         currentHealth = maxHealth;
